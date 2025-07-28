10195752eb0376238db1b736d8936aed161d18e9
Adrian Rodrigo Marchan Collazos <adrian.march23@gmail.com>
2025-07-28 01:01:47 -0500

    Se implemento funcion agregar nombres

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigosLista = ["Jayro", "Sebastian", "Adrian", "Gustavo", "Tuco"];
+
+function agregarAmigo () {
+
+    const nombre = document.getElementById('amigo').value;
+    console.log(nombre)
+
+    if (nombre.trim() !== "") {
+        amigosLista.push(nombre);
+        console.log(amigosLista);
+
+        document.getElementById('amigo').value = ""
+    } else {
+        alert("Por favor, inserte un nombre");
+    }
+
+}
+
