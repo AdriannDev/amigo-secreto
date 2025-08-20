13b97df7c0a50b0fad19e00a01c0e634b7b733b4
Adrian Rodrigo Marchan Collazos <adrian.march23@gmail.com>
2025-08-19 19:42:01 -0500

    Creacion de funciones para la funcionalidad

--- app.js
+++ app.js
@@ -1,19 +1,46 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigosLista = ["Jayro", "Sebastian", "Adrian", "Gustavo", "Tuco"];
+let amigosLista = [];
 
 function agregarAmigo () {
-
     const nombre = document.getElementById('amigo').value;
     console.log(nombre)
 
     if (nombre.trim() !== "") {
+        if (amigosLista.includes(nombre)) {
+            alert("Ese nombre ya esta en la lista, elige otro");
+        }else {
         amigosLista.push(nombre);
+        actualizarAmigos();
         console.log(amigosLista);
-
-        document.getElementById('amigo').value = ""
-    } else {
+        document.getElementById('amigo').value = "";
+        }
+    }else {
         alert("Por favor, inserte un nombre");
     }
-
 }
 
+function actualizarAmigos (){
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    amigosLista.forEach((amigo) => {
+        const nuevoAmigo = document.createElement("li");
+        nuevoAmigo.textContent = amigo;
+        lista.appendChild(nuevoAmigo);
+    })
+}
+
+function sortearAmigo (){
+    if (amigosLista.length > 0){
+        let indice = Math.floor(Math.random() * amigosLista.length);
+        const seleccionado = amigosLista[indice];
+        const resultado = document.getElementById("resultado");
+        resultado.innerHTML = "";
+        const nombre = document.createElement("li");
+        nombre.textContent = seleccionado;
+        resultado.appendChild(nombre);
+        console.log(seleccionado);
+        console.log(indice);
+    }else {
+        alert("No se puede sortear, no existe contenido");
+    }
+}
